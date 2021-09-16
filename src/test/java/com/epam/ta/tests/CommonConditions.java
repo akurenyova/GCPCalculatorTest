2c94617981a5beff1f427ea73781b78a0122a1c7
Anastasiia Kurenyova <anastasiia_kurenyova@epam.com>
2021-09-16 16:02:57 +0300

    add Test Listener

--- src/test/java/com/epam/ta/tests/CommonConditions.java
+++ src/test/java/com/epam/ta/tests/CommonConditions.java
@@ -1,10 +1,13 @@
 package com.epam.ta.tests;
 
 import com.epam.ta.driver.DriverSingleton;
+import com.epam.ta.utils.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners({TestListener.class})
 public class CommonConditions {
 
     protected WebDriver driver;
